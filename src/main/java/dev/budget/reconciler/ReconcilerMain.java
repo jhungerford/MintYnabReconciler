e53c8fc97e9418ed0f4f2bd404f5b387f443201e
John Hungerford <jhungerford@gmail.com>
2014-11-08 08:00:34 -0700

    Moved transactions to a dedicated resources directory.

--- src/main/java/dev/budget/reconciler/ReconcilerMain.java
+++ src/main/java/dev/budget/reconciler/ReconcilerMain.java
@@ -14,19 +14,18 @@ import java.util.List;
 
 public class ReconcilerMain {
 
-	public static final String YNAB_FILE_NAME = "ynab.csv";
-	public static final String MINT_FILE_NAME = "mint.csv";
-
 	public static void main(String[] args) throws Exception {
-		List<YnabTransaction> ynabTransactions = readTransactions(YNAB_FILE_NAME, new YnabTransactionsReader());
-		List<MintTransaction> mintTransactions = readTransactions(MINT_FILE_NAME, new MintTransactionsReader());
+		String transactionsDir = "transactions";
+
+		List<YnabTransaction> ynabTransactions = readTransactions(transactionsDir, "ynab.csv", new YnabTransactionsReader());
+		List<MintTransaction> mintTransactions = readTransactions(transactionsDir, "mint.csv", new MintTransactionsReader());
 
 		System.out.println(ynabTransactions);
 		System.out.println(mintTransactions);
 	}
 
-	private static <T> List<T> readTransactions(String filename, TransactionsReader<T> transactionsReader) throws IOException, URISyntaxException {
-		URL fileUrl = Resources.getResource(filename);
+	private static <T> List<T> readTransactions(String directory, String filename, TransactionsReader<T> transactionsReader) throws IOException, URISyntaxException {
+		URL fileUrl = Resources.getResource(directory + File.separator + filename);
 		try (Reader fileReader = new FileReader(new File(fileUrl.toURI()))) {
 			return transactionsReader.read(fileReader);
 		}
