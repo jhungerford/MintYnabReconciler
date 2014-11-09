7e1107efbff6b1f95d7554c95686e2ac14ada5a3
John Hungerford <jhungerford@gmail.com>
2014-11-09 08:43:13 -0700

    Logback + slf4j for logging

--- src/main/java/dev/budget/reconciler/ReconcilerMain.java
+++ src/main/java/dev/budget/reconciler/ReconcilerMain.java
@@ -8,13 +8,17 @@ import dev.budget.reconciler.csv.handler.ListTransactionHandler;
 import dev.budget.reconciler.model.MintTransaction;
 import dev.budget.reconciler.model.Transaction;
 import dev.budget.reconciler.model.YnabTransaction;
+import org.slf4j.Logger;
 
 import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class ReconcilerMain {
+	private static final Logger log = getLogger(ReconcilerMain.class);
 
 	public static void main(String[] args) throws Exception {
 		String transactionsDir = "transactions";
@@ -22,14 +26,17 @@ public class ReconcilerMain {
 		List<YnabTransaction> ynabTransactions = readTransactions(transactionsDir, "ynab.csv", new YnabTransactionsReader());
 		List<MintTransaction> mintTransactions = readTransactions(transactionsDir, "mint.csv", new MintTransactionsReader());
 
-		System.out.println(ynabTransactions);
-		System.out.println(mintTransactions);
+		log.info("Ynab transactions: {}", ynabTransactions);
+		log.info("Mint transactions: {}", mintTransactions);
 	}
 
 	private static <T extends Transaction> List<T> readTransactions(String directory, String filename, TransactionsReader<T> transactionsReader) throws IOException, URISyntaxException {
 		ListTransactionHandler<T> handler = new ListTransactionHandler<>();
 
-		URL fileUrl = Resources.getResource(directory + File.separator + filename);
+		String resourceName = directory + File.separator + filename;
+		log.debug("Loading transactions from {}", resourceName);
+
+		URL fileUrl = Resources.getResource(resourceName);
 		try (Reader fileReader = new FileReader(new File(fileUrl.toURI()))) {
 			transactionsReader.read(fileReader, handler);
 		}
