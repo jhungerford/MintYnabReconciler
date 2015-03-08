a2b14d1e9336dcca5d58e78d223345766775d304
John Hungerford <jhungerford@gmail.com>
2015-03-08 10:57:44 -0600

    Use futures correctly in PlainResponseFilter.

--- src/main/scala/dev/budget/reconciler/finagle/filter/PlainResponseFilter.scala
+++ src/main/scala/dev/budget/reconciler/finagle/filter/PlainResponseFilter.scala
@@ -7,14 +7,12 @@ import org.jboss.netty.buffer.ChannelBuffers
 import org.jboss.netty.handler.codec.http.{HttpRequest, HttpResponseStatus, DefaultHttpResponse, HttpResponse}
 
 class PlainResponseFilter extends Filter[HttpRequest, HttpResponse, HttpRequest, String] {
+
   override def apply(request: HttpRequest, service: Service[HttpRequest, String]): Future[HttpResponse] = {
-    val response: DefaultHttpResponse = new DefaultHttpResponse(request.getProtocolVersion, HttpResponseStatus.OK)
-
-    val responseBodyFuture: Future[String] = service.apply(request)
-    val responseBody: String = Await.result(responseBodyFuture)
-
-    response.setContent(ChannelBuffers.copiedBuffer(responseBody, Charsets.Utf8))
-
-    Future.value(response) // TODO: this isn't how you program with futures.
+    service.apply(request).flatMap{ str =>
+      val response: DefaultHttpResponse = new DefaultHttpResponse(request.getProtocolVersion, HttpResponseStatus.OK)
+      response.setContent(ChannelBuffers.copiedBuffer(str, Charsets.Utf8))
+      Future.value(response)
+    }
   }
 }
