96130c07e773cee99b94e2e32929d3f2a4ac6ffa
John Hungerford <jhungerford@gmail.com>
2015-03-14 09:28:56 -0600

    Removed unnecessary ReconcilerConfiguration.

--- src/main/java/dev/budget/reconciler/config/ReconcilerConfiguration.java
+++
@@ -1,6 +0,0 @@
-package dev.budget.reconciler.config;
-
-import io.dropwizard.Configuration;
-
-public class ReconcilerConfiguration extends Configuration {
-}
--- src/main/scala/dev/budget/reconciler/ReconcilerApplication.scala
+++ src/main/scala/dev/budget/reconciler/ReconcilerApplication.scala
@@ -2,21 +2,21 @@ package dev.budget.reconciler
 
 import com.massrelevance.dropwizard.ScalaApplication
 import dev.budget.reconciler.api.HelloResource
-import dev.budget.reconciler.config.ReconcilerConfiguration
 import dev.budget.reconciler.es.ManagedElasticSearch
 import dev.budget.reconciler.health.ElasticSearchHealth
+import io.dropwizard.Configuration
 import io.dropwizard.assets.AssetsBundle
 import io.dropwizard.setup.{Bootstrap, Environment}
 import scaldi.{Injectable, Injector}
 
-class ReconcilerApplication(implicit inj: Injector) extends ScalaApplication[ReconcilerConfiguration] with Injectable {
+class ReconcilerApplication(implicit inj: Injector) extends ScalaApplication[Configuration] with Injectable {
 
-  override def initialize(bootstrap: Bootstrap[ReconcilerConfiguration]) = {
+  override def initialize(bootstrap: Bootstrap[Configuration]) = {
     bootstrap.addBundle(new AssetsBundle("/web", "/web", "", "web"))
     bootstrap.addBundle(new AssetsBundle("/html", "", "index.html", "html"))
   }
 
-  override def run(configuration: ReconcilerConfiguration, environment: Environment) = {
+  override def run(configuration: Configuration, environment: Environment) = {
     environment.jersey.setUrlPattern("/api/*")
     environment.lifecycle().manage( inject [ManagedElasticSearch] )
     environment.healthChecks().register("ElasticSearch", inject [ElasticSearchHealth])
